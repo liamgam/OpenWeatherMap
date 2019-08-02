21297355b6c46ae688e1e84df3118989670310a6
liamgam <liam.liamgamble@gmail.com>
2019-08-02 13:30:06 -0700

    added starter project

---
+++ OpenWeatherMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherMap
+//
+//  Created by Rinni Swift on 8/2/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
