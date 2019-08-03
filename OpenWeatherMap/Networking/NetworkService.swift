3d8bebaa48a2b6df5cca050e2453d3c2876db270
liamgam <liam.liamgamble@gmail.com>
2019-08-02 22:55:23 -0700

    added networkService class

---
+++ OpenWeatherMap/Networking/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  OpenWeatherMap
+//
+//  Created by Rinni Swift on 8/2/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+class NetworkService {
+    
+}
