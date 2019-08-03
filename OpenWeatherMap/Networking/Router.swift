8d4526099c20c3633fcd28d5daa4f755fb971e57
liamgam <liam.liamgamble@gmail.com>
2019-08-02 22:55:41 -0700

    added Router enum with all routes

--- OpenWeatherMap/Networking/Router.swift
+++ OpenWeatherMap/Networking/Router.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+enum Router {
+    
+    case getWeatherInfo(city: String)
+    
+    var apiKey: String? {
+        let infoFile = getPlist()
+        if let infoFile = infoFile {
+            return infoFile["APIKey"]! as! String
+        }
+        return nil
+    }
+    
+    var scheme: String {
+        switch self {
+        case .getWeatherInfo:
+            return "https"
+        }
+    }
+    
+    var host: String {
+        switch self {
+        case .getWeatherInfo:
+            return "samples.openweathermap.org"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getWeatherInfo:
+            return "/data/2.5/forecast"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        switch self {
+        case .getWeatherInfo(let city):
+            return[URLQueryItem(name: "q", value: city),
+                   URLQueryItem(name: "APPID", value: apiKey!)]
+        }
+    }
+        
+    
+    
+}
