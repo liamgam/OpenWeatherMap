65287d32a53ecd04178d56f91d15fde4213cb682
liamgam <liam.liamgamble@gmail.com>
2019-08-04 12:15:23 -0700

    added network function to get weather data

--- OpenWeatherMap/Networking/NetworkService.swift
+++ OpenWeatherMap/Networking/NetworkService.swift
@@ -10,4 +10,40 @@ import Foundation
 
 class NetworkService {
     
+    class func getWeatherData<T: Codable>(router: Router, completion: @escaping (Result<[T], Error>) -> ()) {
+        
+        var components = URLComponents()
+        components.scheme = router.scheme
+        components.host = router.host
+        components.path = router.path
+        components.queryItems = router.parameters
+        
+        let session = URLSession(configuration: .default)
+        guard let url = components.url else { print("invalid url"); return }
+        let request = URLRequest(url: url)
+        
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            
+            guard error == nil else {
+                print(error?.localizedDescription)
+                completion(.failure(error!))
+                return
+            }
+            guard response != nil else {
+                print("no response from \(request)")
+                return
+            }
+            guard let data = data else {
+                print("no data")
+                return
+            }
+            
+            let responseObj = try! JSONDecoder().decode([T].self, from: data)
+            DispatchQueue.main.async {
+                completion(.success(responseObj))
+            }
+        }
+        dataTask.resume()
+    }
+    
 }
