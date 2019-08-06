92addb228399852c9063d234f834952673026d5b
liamgam <liam.liamgamble@gmail.com>
2019-08-06 16:11:06 -0700

    moeved files

---
+++ OpenWeatherMap/Controller/WeatherInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherInfoViewController.swift
+//  OpenWeatherMap
+//
+//  Created by Rinni Swift on 8/6/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class WeatherInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
